3cdf7a95dd172e75e711481e0c8f60f0670ea6b8
papalima <pi.laloy@laposte.net>
2024-01-30 07:29:47 +0100

    MODIFY : Optimize imports ++ beautify code ++ adding default value over method generate password2 to prevent null pointer exeption.

--- src/main/java/papalima/passwordgenerator/controller/PasswordGeneratorController.java
+++ src/main/java/papalima/passwordgenerator/controller/PasswordGeneratorController.java
@@ -1,6 +1,8 @@
 package papalima.passwordgenerator.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import papalima.passwordgenerator.model.PasswordRecord;
 import papalima.passwordgenerator.services.PasswordGeneratorService;
 
@@ -10,18 +12,17 @@ public class PasswordGeneratorController {
 
     @GetMapping(path = "/generatePassword", produces="application/json")
     public PasswordRecord generateNewPassword() {
-        System.out.println("called");
         return generator.generatePassword();
     }
 
 
-    @GetMapping(path = "/generatePassword2")
-    public PasswordRecord generateNewPassword (
-            @RequestParam(name="passwordSize", required = false) int passwordSize,
-            @RequestParam(name="specialChars", required = false) boolean specialChars,
-            @RequestParam(name="upperCase", required = false) boolean upperCase,
-            @RequestParam(name="withNumbers", required = false) boolean withNumbers
-    ){
+    @GetMapping(path = "/generatePassword2", produces = "application/json")
+    public PasswordRecord generateNewPassword(
+            @RequestParam(name = "passwordSize", required = false, defaultValue = "8") Integer passwordSize,
+            @RequestParam(name = "specialChars", required = false, defaultValue = "true") Boolean specialChars,
+            @RequestParam(name = "upperCase", required = false, defaultValue = "true") Boolean upperCase,
+            @RequestParam(name = "withNumbers", required = false, defaultValue = "true") Boolean withNumbers
+    ) {
         return generator.generatePasswordWithParam(passwordSize, specialChars, upperCase, withNumbers);
     }
 
