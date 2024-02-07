581b3e6db4f85f8aa6f9d4ea7663c1650f6493cd
papalima <pi.laloy@laposte.net>
2024-02-07 15:27:35 +0100

    DELETE : IntervalReccord deleted,it make more sense using an enum for this usecase.

--- src/main/java/papalima/passwordgenerator/model/IntervalRecord.java
+++
@@ -1,7 +0,0 @@
-package papalima.passwordgenerator.model;
-
-public record IntervalRecord(int minValue, int maxValue) {
-    public int getRange() {
-        return maxValue - minValue + 1;
-    }
-}
